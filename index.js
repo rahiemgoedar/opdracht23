4b6d41bf5909c91d8ef90a559484c6b08a507031
rahiemgoedar <79698303+rahiemgoedar@users.noreply.github.com>
2021-04-17 14:54:40 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const makePizza = function () {
+    console.log('create dough with flower');
+    console.log('put tomato sauce on it');
+    console.log('sprinkle with cheese');
+};
+/* makePizza();
+makePizza();
+makePizza(); */
+
+const makeSushi = function () {
+    console.log('get sticky rice');
+    console.log('put seaweed and fish in it');
+    console.log('roll it and cut into pieces');
+};
+
+/* makeSushi(); */
+
+makeSushi() + makeSushi() + makeSushi() + makePizza() + makePizza(); //er is vast een snellere manier
